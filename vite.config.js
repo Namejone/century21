8719929bb7c29e6b3a8cccc4673e9e292edbe19a
namejone <devnamejone@gmail.com>
2026-04-08 12:59:57 +0600

    Project Status updated

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,15 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        aVendre: 'a-vendre.html',
+      },
+    },
+  },
 });
+
+
+
